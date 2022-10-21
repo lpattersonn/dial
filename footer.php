23d0fa88d55b9b28c9aeb26ad1ec49e6c64d2d3f
Leshan <leshan0627@gmail.com>
2022-10-20 21:40:14 -0400

    style: Update footer style

--- footer.php
+++ footer.php
@@ -15,25 +15,34 @@
 <footer id="site-footer" class="header-footer-group">
 
     <div class="section-inner">
+        <div class="section-inner__one">
+            <div class="section-inner__one__text">
+                <h2>Stay In The Know</h2>
+                <p>Sign up to receive launch updates and more!</p>
+            </div>
+            <div class="section-inner-one__form">
+                <?php include "partials/scripts/subscription-form.php"; ?>
+            </div>
+        </div>
+        <div class="section-inner__two">
+            <?php
+                $sitelogo = get_field('site_logo', 'options');
+                $logo_link_url = get_field('logo_link_url', 'options');
+                echo '<a href='.$logo_link_url.'><img class="footer-site-logo" src='.$sitelogo['url'].' alt="" /></a>';
+            ?>
 
-        <?php
-            $sitelogo = get_field('site_logo', 'options');
-            $logo_link_url = get_field('logo_link_url', 'options');
-            echo '<a href='.$logo_link_url.'><img class="footer-site-logo" src='.$sitelogo['url'].' alt="" /></a>';
-        ?>
+            <div class="footer-credits">
 
-        <div class="footer-credits">
-
-            <p class="footer-copyright">Copyright &copy;
-                <?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
-								_x( 'Y', 'copyright date format', 'twentytwenty' )
-							);
-							?>. All rights reserved.
-            </p><!-- .footer-copyright -->
-        </div><!-- .footer-credits -->
-
+                <p class="footer-copyright">Copyright &copy;
+                    <?php
+                                echo date_i18n(
+                                    /* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
+                                    _x( 'Y', 'copyright date format', 'twentytwenty' )
+                                );
+                                ?>. All rights reserved.
+                </p><!-- .footer-copyright -->
+            </div><!-- .footer-credits -->
+        </div>
     </div><!-- .section-inner -->
 
 </footer><!-- #site-footer -->
---
+++ partials/scripts/subscription-form.php
@@ -0,0 +1,29 @@
+<!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+    <form
+        action="https://sg-host.us10.list-manage.com/subscribe/post?u=a812a60907b11b75f04fb1261&amp;id=eccfdf679b&amp;f_id=008231e2f0"
+        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self">
+        <div id="mc_embed_signup_scroll">
+            <div class="mc-field-group">
+                </label>
+                <input type="email" value="" name="EMAIL" placeholder="Email Address" class="required email"
+                    id="mce-EMAIL" required>
+                <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+            </div>
+            <div id="mce-responses" class="clear foot">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
+                    name="b_a812a60907b11b75f04fb1261_eccfdf679b" tabindex="-1" value=""></div>
+            <div class="optionalParent">
+                <div class="clear foot">
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<!--End mc_embed_signup-->
